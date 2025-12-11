55d9f40fa8284beb313d4344f0f8a81b14e05593
Fabricio Banda <fabribanda438@gmail.com>
2025-12-11 15:08:27 -0600

    Initial Commit

---
+++ GitHubProfileViewer/GitHubProfileViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProfileViewerApp.swift
+//  GitHubProfileViewer
+//
+//  Created by Fabricio Banda Hernandez on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubProfileViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
